9240b002d520ca877bf04fcaf115c30b92adb3f1
Max T <dev@maxthakur.xyz>
2023-08-28 14:36:29 -0400

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,1 +1,8 @@
-pnpm install --frozen-lockfile && npm run build && pm2 restart "Jobs"
+# pull from github
+git pull
+# make sure libs are up-to-date
+pnpm install --frozen-lockfile 
+# build App
+npm run build
+# restart the service in pm2 so updates
+pm2 restart "Jobs"
